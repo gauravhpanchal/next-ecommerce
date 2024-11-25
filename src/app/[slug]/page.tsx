93c20d6752dc57422bb36cd7402a92737e34a19b
Gaurav <gauravhpanchal2@gmail.com>
2024-11-25 11:39:43 +0530

    pageProps errors fixed

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -29,15 +29,15 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
         <h1 className="text-4xl font-medium">{product.name}</h1>
         <p className="text-gray-500">{product.description}</p>
         <div className="h-[2px] bg-gray-100" />
-        {product.price?.price === product.price?.discountedPrice ? (
-          <h2 className="font-medium text-2xl">${product.price?.price}</h2>
+        {product.price?.price === product.priceData?.discountedPrice ? (
+          <h2 className="font-medium text-2xl">${product.priceData?.price}</h2>
         ) : (
           <div className="flex items-center gap-4">
             <h3 className="text-xl text-gray-500 line-through">
               ${product.price?.price}
             </h3>
             <h2 className="font-medium text-2xl">
-              ${product.price?.discountedPrice}
+              ${product.priceData?.discountedPrice}
             </h2>
           </div>
         )}
