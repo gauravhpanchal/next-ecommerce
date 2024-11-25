3d12dd7b08294a2c037123d230d3602d363b200d
Gaurav <gauravhpanchal2@gmail.com>
2024-11-25 11:37:45 +0530

    type error fixed

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,13 +3,8 @@ import CustomizeProducts from "@/components/CustomizeProducts/CustomizeProducts"
 import ProductImages from "@/components/ProductImages/ProductImages";
 import { wixClientServer } from "@/lib/wixClientServer";
 import { notFound } from "next/navigation";
-import React from "react";
 
-interface ProductDetailsPageProps {
-  params: { slug: string };
-}
-
-const ProductDetailsPage = async ({ params }: ProductDetailsPageProps) => {
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
   const wixClient = await wixClientServer();
 
   const products = await wixClient.products
@@ -25,60 +20,52 @@ const ProductDetailsPage = async ({ params }: ProductDetailsPageProps) => {
 
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
-      {/* Images */}
+      {/* IMG */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
         <ProductImages items={product.media?.items} />
       </div>
-      {/* Texts */}
+      {/* TEXTS */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        {/* Product Name */}
         <h1 className="text-4xl font-medium">{product.name}</h1>
-        {/* Product Description */}
         <p className="text-gray-500">{product.description}</p>
         <div className="h-[2px] bg-gray-100" />
-        {/* Pricing */}
-        {product.priceData?.price === product.priceData?.discountedPrice ? (
-          <h2 className="font-medium text-2xl">
-            {product.priceData?.formatted?.price}
-          </h2>
+        {product.price?.price === product.price?.discountedPrice ? (
+          <h2 className="font-medium text-2xl">${product.price?.price}</h2>
         ) : (
           <div className="flex items-center gap-4">
             <h3 className="text-xl text-gray-500 line-through">
-              {product.priceData?.formatted?.price}
+              ${product.price?.price}
             </h3>
             <h2 className="font-medium text-2xl">
-              {product.priceData?.formatted?.discountedPrice}
+              ${product.price?.discountedPrice}
             </h2>
           </div>
         )}
-
         <div className="h-[2px] bg-gray-100" />
-        {/* Customize Options */}
         {product.variants && product.productOptions ? (
           <CustomizeProducts
-            productId={product._id || ""}
-            variants={product.variants || []}
-            productOptions={product.productOptions || []}
+            productId={product._id!}
+            variants={product.variants}
+            productOptions={product.productOptions}
           />
         ) : (
           <Add
             productId={product._id!}
-            variantId="00000000-000000-000000-000000000001"
+            variantId="00000000-0000-0000-0000-000000000000"
             stockNumber={product.stock?.quantity || 0}
           />
         )}
-        {/* Add to Cart */}
         <div className="h-[2px] bg-gray-100" />
-        {/* Terms and Condition */}
-        {product.additionalInfoSections?.map((item, idx) => (
-          <div className="text-sm" key={idx}>
-            <h4 className="font-medium mb-4">{item.title}</h4>
-            <p>{item.description}</p>
+        {product.additionalInfoSections?.map((section: any) => (
+          <div className="text-sm" key={section.title}>
+            <h4 className="font-medium mb-4">{section.title}</h4>
+            <p>{section.description}</p>
           </div>
         ))}
+        <div className="h-[2px] bg-gray-100" />
       </div>
     </div>
   );
 };
 
-export default ProductDetailsPage;
+export default SinglePage;
